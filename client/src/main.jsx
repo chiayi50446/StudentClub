7dc9bdc2b82e6498caf56b7f7b39e746a78e3ae4
YuHsuanChen <ddy20824@gmail.com>
2024-11-08 23:24:21 -0500

    Add basic codes/packages & fix package problems

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />)
---
+++ client/user/Users.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import {useState} from 'react'
+import {useEffect} from 'react'
+import { makeStyles } from '@mui/styles'
+import Paper from '@mui/material/Paper'
+import List from '@mui/material/List'
+import {list} from './api-user.js'
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@mui/material/Link'
+import ListItem from '@mui/material/ListItem'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction'
+import IconButton from '@mui/material/IconButton'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
+import ArrowForward from '@mui/icons-material/ArrowForward';
+
+const useStyles = makeStyles(theme => ({
+    card: {
+      // Define your card styles here
+    },
+    textField: {
+      // Define your text field styles here
+    },
+    error: {
+      // Define your error icon styles here
+    },
+    submit: {
+      // Define your submit button styles here
+    },
+    title: {
+      // Define your title styles here
+    },
+    root: {
+        // Define your root styles here
+      },
+  }));
+
+export default function Users() {
+
+    const [users, setUsers] = useState([])
+useEffect(() => {
+    const abortController = new AbortController()
+    const signal = abortController.signal
+    list(signal).then((data) => {
+      console.log(data)
+if (data && data.error) { 
+console.log(data.error)
+} else { 
+    console.log(data)
+setUsers(data)
+} 
+})
+return function cleanup(){ 
+abortController.abort()
+} 
+}, [])
+
+  
+    const classes = useStyles()
+return (
+<Paper className={classes.root} elevation={4}>
+<Typography variant="h6" className={classes.title}> 
+All Users
+</Typography> 
+<List dense>
+{users.map((item, i) => { 
+  return  <Link component={RouterLink} to={"/user/" + item._id} key={i}>
+    
+<ListItem button> 
+<ListItemAvatar>
+<Avatar> 
+
+</Avatar>
+</ListItemAvatar>
+<ListItemText primary={item.name}/> 
+<ListItemSecondaryAction>
+<IconButton>
+<ArrowForward/> 
+</IconButton>
+</ListItemSecondaryAction> 
+</ListItem>
+</Link> 
+})} 
+</List>
+</Paper>
+)
+}
+
+
