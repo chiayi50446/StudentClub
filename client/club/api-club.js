c59c4a8bf6c5ab3efed7bbe20f6bc66d4330dc0a
Chiayi <angel05446a@gmail.com>
2024-11-25 15:09:45 -0500

    Add clubList in user profile

--- client/club/api-club.js
+++ client/club/api-club.js
@@ -1,121 +1,123 @@
 // Function to create a new club
 const create = async (club) => {
-    try {
-        // Send a POST request to create the club
-        let response = await fetch('/api/clubs/', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(club)
-        });
+  try {
+    // Send a POST request to create the club
+    let response = await fetch("/api/clubs/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(club),
+    });
 
-        // Parse the response JSON
-        const data = await response.json();
+    // Parse the response JSON
+    const data = await response.json();
 
-        // Check if the response is successful (status code 2xx)
-        if (response.ok) {
-            alert('Club created successfully!');
-            return data; // Return the data of the newly created club
-        } else {
-            alert('Error creating club: ' + (data.message || 'Something went wrong.'));
-            return null; // Return null if creation fails
-        }
-    } catch (err) {
-        console.error('Error:', err);
-        alert('An error occurred while creating the club. Please try again later.');
-        return null; // Return null in case of any error
+    // Check if the response is successful (status code 2xx)
+    if (response.ok) {
+      alert("Club created successfully!");
+      return data; // Return the data of the newly created club
+    } else {
+      alert(
+        "Error creating club: " + (data.message || "Something went wrong.")
+      );
+      return null; // Return null if creation fails
     }
-}
+  } catch (err) {
+    console.error("Error:", err);
+    alert("An error occurred while creating the club. Please try again later.");
+    return null; // Return null in case of any error
+  }
+};
 
 // Function to list all clubs
-const list = async (signal) => {
-    try {
-        let response = await fetch('/api/clubs/', {
-            method: 'GET',
-            signal: signal
-        });
+const list = async (signal, query) => {
+  try {
+    let response = await fetch(`/api/clubs?clubList=${query}`, {
+      method: "GET",
+      signal: signal,
+    });
 
-        if (!response.ok) {
-            throw new Error('Failed to fetch clubs');
-        }
-
-        // Return the list of clubs
-        return await response.json(); 
-    } catch (err) {
-        console.error('Error fetching clubs:', err);
-        return null; // Return null in case of error
+    if (!response.ok) {
+      throw new Error("Failed to fetch clubs");
     }
-}
+
+    // Return the list of clubs
+    return await response.json();
+  } catch (err) {
+    console.error("Error fetching clubs:", err);
+    return null; // Return null in case of error
+  }
+};
 
 // Function to read a specific club's details by ID
 const read = async (params, signal) => {
-    try {
-        let response = await fetch(`/api/clubs/${params.clubId}`, {
-            method: 'GET',
-            signal: signal,
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        });
+  try {
+    let response = await fetch(`/api/clubs/${params.clubId}`, {
+      method: "GET",
+      signal: signal,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error(`Error fetching club with ID ${params.clubId}`);
-        }
-
-        return await response.json(); // Return the club data
-    } catch (err) {
-        console.error('Error reading club:', err);
-        return null; // Return null in case of error
+    if (!response.ok) {
+      throw new Error(`Error fetching club with ID ${params.clubId}`);
     }
-}
+
+    return await response.json(); // Return the club data
+  } catch (err) {
+    console.error("Error reading club:", err);
+    return null; // Return null in case of error
+  }
+};
 
 // Function to update an existing club's details
 const update = async (params, club) => {
-    try {
-        let response = await fetch(`/api/clubs/${params.clubId}`, {
-            method: 'PUT',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(club)
-        });
+  try {
+    let response = await fetch(`/api/clubs/${params.clubId}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(club),
+    });
 
-        if (!response.ok) {
-            throw new Error(`Error updating club with ID ${params.clubId}`);
-        }
-
-        return await response.json(); // Return the updated club data
-    } catch (err) {
-        console.error('Error updating club:', err);
-        return null; // Return null in case of error
+    if (!response.ok) {
+      throw new Error(`Error updating club with ID ${params.clubId}`);
     }
-}
+
+    return await response.json(); // Return the updated club data
+  } catch (err) {
+    console.error("Error updating club:", err);
+    return null; // Return null in case of error
+  }
+};
 
 // Function to remove a club (delete)
 const remove = async (params) => {
-    try {
-        let response = await fetch(`/api/clubs/${params.clubId}`, {
-            method: 'DELETE',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        });
+  try {
+    let response = await fetch(`/api/clubs/${params.clubId}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error(`Error deleting club with ID ${params.clubId}`);
-        }
-
-        return await response.json(); // Return the response of the delete operation
-    } catch (err) {
-        console.error('Error removing club:', err);
-        return null; // Return null in case of error
+    if (!response.ok) {
+      throw new Error(`Error deleting club with ID ${params.clubId}`);
     }
-}
+
+    return await response.json(); // Return the response of the delete operation
+  } catch (err) {
+    console.error("Error removing club:", err);
+    return null; // Return null in case of error
+  }
+};
 
 // Export the functions to be used in other parts of the app
 export { create, list, read, update, remove };
--- server/controllers/club.controller.js
+++ server/controllers/club.controller.js
@@ -1,72 +1,78 @@
-import Club from '../models/club.model.js';
-import extend from 'lodash/extend.js';
-import errorHandler from './error.controller.js';
+import Club from "../models/club.model.js";
+import extend from "lodash/extend.js";
+import errorHandler from "./error.controller.js";
 
 const create = async (req, res) => {
-    console.log(req.body);
-    const club = new Club(req.body);
-    try {
-        await club.save();
-        return res.status(200).json({
-            message: "Successfully create club!"
-        });
-    } catch (err) {
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err)
-        });
-    }
-}
+  console.log(req.body);
+  const club = new Club(req.body);
+  try {
+    await club.save();
+    return res.status(200).json({
+      message: "Successfully create club!",
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
 const list = async (req, res) => {
-    try {
-        let clubs = await Club.find().select('name description status type leadership pictureUri contactInfo created updated');
-        res.json(clubs);
-    } catch (err) {
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err)
-        });
+  try {
+    const query = {};
+    if (req.query) {
+      query._id = { $in: req.query.clubList.split(",") };
     }
-}
-const clubByID = async (req, res, next, id) => {
-    try {
-        let club = await Club.findById(id);
-        if (!club)
-            return res.status('400').json({
-                error: "Club not found"
-            });
-        req.profile = club;
-        next();
-    } catch (err) {
-        return res.status('400').json({
-            error: "Could not retrieve club"
-        });
-    }
-}
-const read = (req, res) => {
-    return res.json(req.profile);
-}
-const update = async (req, res) => {
-    try {
-        let club = req.profile;
-        club = extend(club, req.body);
-        club.updated = Date.now();
-        await club.save();
-        res.json(club);
-    } catch (err) {
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err)
-        });
-    }
-}
-const remove = async (req, res) => {
-    try {
-        let club = req.profile;
-        let deletedClub = await club.deleteOne();
-        res.json(deletedClub);
-    } catch (err) {
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err)
-        });
-    }
-}
-export default { create, clubByID, read, list, remove, update }
 
+    let clubs = await Club.find().select(
+      "name description status type leadership pictureUri contactInfo created updated"
+    );
+    res.json(clubs);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
+const clubByID = async (req, res, next, id) => {
+  try {
+    let club = await Club.findById(id);
+    if (!club)
+      return res.status("400").json({
+        error: "Club not found",
+      });
+    req.profile = club;
+    next();
+  } catch (err) {
+    return res.status("400").json({
+      error: "Could not retrieve club",
+    });
+  }
+};
+const read = (req, res) => {
+  return res.json(req.profile);
+};
+const update = async (req, res) => {
+  try {
+    let club = req.profile;
+    club = extend(club, req.body);
+    club.updated = Date.now();
+    await club.save();
+    res.json(club);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
+const remove = async (req, res) => {
+  try {
+    let club = req.profile;
+    let deletedClub = await club.deleteOne();
+    res.json(deletedClub);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
+export default { create, clubByID, read, list, remove, update };
