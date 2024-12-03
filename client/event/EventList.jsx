7909dc0643b8f7b0217f866dc8148fdc4c79b3cc
Chiayi <angel05446a@gmail.com>
2024-12-03 17:06:39 -0500

    EventList layout

--- client/event/EventList.jsx
+++ client/event/EventList.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { listEvents, updateEvent, deleteEvent } from "./api-event.js"; // Import API functions
-import { list } from "../club/api-club.js"; // Import clubs function
+import { list as listClub } from "../club/api-club.js"; // Import clubs function
 import CircularProgress from "@mui/material/CircularProgress";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
@@ -18,10 +17,33 @@ import DialogTitle from "@mui/material/DialogTitle";
 import AddIcon from "@mui/icons-material/Add";
 import { useNavigate } from "react-router-dom";
 import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
+import Container from "@mui/material/Container";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import Avatar from '@mui/material/Avatar';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Rating from '@mui/material/Rating';
+import Grid from '@mui/material/Grid2';
+import IconButton from '@mui/material/IconButton';
+import SendIcon from '@mui/icons-material/Send';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import auth from "../lib/auth-helper.js";
 
 const EventList = () => {
+  const [expanded, setExpanded] = React.useState('panel1');
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setRating(0);
+    setComment('');
+    setExpanded(newExpanded ? panel : false);
+  };
   const navigate = useNavigate();
+  const [rating, setRating] = React.useState(0);
+  const [comment, setComment] = React.useState('');
   const [events, setEvents] = useState([]);
   const [filteredEvents, setFilteredEvents] = useState([]);
   const [clubs, setClubs] = useState([]); // State for clubs
@@ -41,7 +63,7 @@ const EventList = () => {
       try {
         const [eventsData, clubsData] = await Promise.all([
           listEvents(),
-          list(),
+          listClub(),
         ]);
         setEvents(eventsData);
         setClubs(clubsData);
@@ -94,11 +116,11 @@ const EventList = () => {
     applyFilter(); // Apply the filter whenever the filter state changes
   }, [filter, events]);
 
-  const handleEditClick = (event) => {
+  const handleEditClick = (e, event) => {
+    e.stopPropagation();
     setEditingEvent(event);
     setUpdatedEvent({
       ...event,
-      club: event.club?._id || "",
       date: event.date.split("T")[0] || "", // Format date to YYYY-MM-DD for the dialog input
       organizer: event.organizer || "",
     }); // Pre-fill the dialog with existing data
@@ -116,6 +138,30 @@ const EventList = () => {
     setEditingEvent(null);
   };
 
+  const handleRating = async(event) => {
+    setRating(0);
+    setComment('');
+    if (!event.rating) {
+      event.rating = [];
+    }
+    event.rating.push({ userId: auth.isAuthenticated().user._id, userName:auth.isAuthenticated().user.name, stars: rating, comment: comment });
+    try {
+      const updated = await updateEvent(event._id, event);
+      setEvents((prevItems) =>{
+        return prevItems.map((item) =>
+          item._id === event._id  ? updated : item
+        )}
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getAverage = (array) => {
+    console.log(array);
+    return array.reduce((sum, currentValue) => sum + currentValue.stars, 0) / array.length;
+  };
+
   const handleUpdateEvent = async () => {
     try {
       // Ensure the date is correctly formatted before sending the update request
@@ -126,21 +172,26 @@ const EventList = () => {
       });
 
       // After updating, refetch the events to refresh the list
-      const updatedEvents = await listEvents();
-      setEvents(updatedEvents);
-      setFilteredEvents(updatedEvents); // Also update filtered events
+      setEvents((prevItems) =>{
+        return prevItems.map((item) =>
+          item._id === updatedEvent._id  ? updatedEvent : item
+        )}
+      );
       handleCloseDialog();
     } catch (err) {
       alert("Failed to update event");
     }
   };
 
-  const handleDeleteEvent = async (eventId) => {
+  const handleDeleteEvent = async (e, eventId) => {
+    e.stopPropagation();
     try {
       await deleteEvent(eventId);
-      const updatedEvents = await listEvents();
-      setEvents(updatedEvents);
-      setFilteredEvents(updatedEvents); // Update the filtered events list
+      setEvents((prevItems) =>{
+        return prevItems.filter((item) =>
+          item._id !== eventId
+        )}
+      );
     } catch (err) {
       alert("Failed to delete event");
     }
@@ -154,12 +205,20 @@ const EventList = () => {
     }));
   };
 
+  const handleClearFilterChange = (name) => {
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      [name]: '',
+    }));
+  };
+
   const handleClubChange = (e) => {
-    const selectedClubId = e.target.value;
-    const selectedClub = clubs.find((club) => club._id === selectedClubId);
+    const {club} = updatedEvent;
+    club._id = e.target.value,
+    club.name = clubs.find(x => x._id === e.target.value).name
     setUpdatedEvent({
       ...updatedEvent,
-      club: selectedClub,
+      club: club
     });
   };
 
@@ -189,9 +248,42 @@ const EventList = () => {
       </Paper>
     );
   }
+  const DateAvatar = ({ date }) => {
+    const formattedDate = new Date(date).toLocaleDateString('en-US', {
+      timeZone: 'UTC',
+      month: 'short',
+      day: 'numeric',
+    });
+  
+    const [month, day] = formattedDate.split(' ');
+  
+    return (
+      <Avatar
+        sx={{
+          width: 40,
+          height: 40,
+          backgroundColor: '#1976d2',
+          color: 'white',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 0,
+          mt:1
+        }}
+      >
+        <Typography variant="caption" sx={{ fontSize: 12 }}>
+          {month.toUpperCase()}
+        </Typography>
+        <Typography variant="h6" sx={{ fontWeight: 'bold', lineHeight: 1 }}>
+          {day}
+        </Typography>
+      </Avatar>
+    );
+  };
+  
 
   return (
-    <Box sx={{ maxWidth: 600, margin: "auto", p: 2 }}>
+    <Container maxWidth="lg">
       <ListItem alignItems="flex-start">
         <ListItemText>
           <Typography variant="h5" sx={{ mb: 2 }}>
@@ -200,20 +292,19 @@ const EventList = () => {
         </ListItemText>
         <Button
           variant="contained"
-          size="small"
-          sx={{ mt: 1 }}
+          sx={{ mb: 2 }}
           startIcon={<AddIcon />}
           onClick={() => handleAddClick()}
         >
           Add
         </Button>
       </ListItem>
-
       {/* Filter Section */}
       <Box sx={{ display: "flex", gap: 2, marginBottom: 2 }}>
         <TextField
           label="Search by Title"
           name="title"
+            size="small"
           value={filter.title}
           onChange={handleFilterChange}
           fullWidth
@@ -222,13 +313,11 @@ const EventList = () => {
           select
           label="Filter by Club"
           name="club"
+          size="small"
           value={filter.club}
           onChange={handleFilterChange}
           fullWidth
         >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
           {clubs.map((club) => (
             <MenuItem key={club._id} value={club._id}>
               {club.name}
@@ -238,6 +327,7 @@ const EventList = () => {
         <TextField
           label="Search by Location"
           name="location"
+            size="small"
           value={filter.location}
           onChange={handleFilterChange}
           fullWidth
@@ -246,97 +336,75 @@ const EventList = () => {
           label="Search by Date"
           type="date"
           name="date"
+            size="small"
           value={filter.date}
           onChange={handleFilterChange}
           InputLabelProps={{ shrink: true }}
           fullWidth
         />
       </Box>
-
-      {filteredEvents.length === 0 ? (
-        <Typography variant="body1">No events found.</Typography>
-      ) : (
-        <List>
-          {filteredEvents.map((event) => (
-            <React.Fragment key={event._id}>
-              <ListItem alignItems="flex-start">
-                <ListItemText
-                  primary={event.title}
-                  secondary={
-                    <>
-                      {event.date && (
-                        <Typography
-                          component="span"
-                          variant="body2"
-                          color="textSecondary"
-                        >
-                          Date:{" "}
-                          {(() => {
-                            const eventDate = new Date(event.date);
-                            // Add one day to the event date
-                            eventDate.setDate(eventDate.getDate());
-                            // Format the date as YYYY-MM-DD
-                            const formattedDate = eventDate
-                              .toISOString()
-                              .split("T")[0];
-                            return `${formattedDate} | Location: ${event.location}`;
-                          })()}
-                          <br />
-                        </Typography>
-                      )}
-                      <Typography
-                        component="span"
-                        variant="body2"
-                        color="textSecondary"
-                      >
-                        Organizer: {event.organizer}
-                        <br />
-                      </Typography>
-                      {event.description && (
-                        <Typography
-                          component="span"
-                          variant="body2"
-                          color="textSecondary"
-                        >
-                          Description: {event.description}
-                          <br />
-                        </Typography>
-                      )}
-                      {event.club && (
-                        <Typography
-                          component="span"
-                          variant="body2"
-                          color="textSecondary"
-                        >
-                          Club: {event.club.name}
-                        </Typography>
-                      )}
-
-                      {event.rating &&
-                        event.rating.map((item, i) => {
-                          <Typography
-                            component="span"
-                            variant="body2"
-                            color="textSecondary"
-                          >
-                            {event.description}
-                          </Typography>;
-                        })}
-                    </>
-                  }
-                />
-                <Button
+      <div>
+          <Stack direction="row" sx={{ mb: 1 }}>
+            {filter.title && (
+              <Chip
+                size="small"
+                name="title"
+                label={filter.title}
+                variant="outlined"
+                onClick={() => handleClearFilterChange("title")}
+                onDelete={() => handleClearFilterChange("title")}
+              />
+            )}
+            {filter.club && (
+              <Chip
+                size="small"
+                label={clubs.find((member) => member._id === filter.club).name}
+                name="club"
+                variant="outlined"
+                onClick={() => handleClearFilterChange("club")}
+                onDelete={() => handleClearFilterChange("club")}
+              />
+            )}
+            {filter.location && (
+              <Chip
+                size="small"
+                name="location"
+                label={filter.location}
+                variant="outlined"
+                onClick={() => handleClearFilterChange("location")}
+                onDelete={() => handleClearFilterChange("location")}
+              />
+            )}
+            {filter.date && (
+              <Chip
+                size="small"
+                name="date"
+                label={filter.date}
+                variant="outlined"
+                onClick={() => handleClearFilterChange("date")}
+                onDelete={() => handleClearFilterChange("date")}
+              />
+            )}
+          </Stack>
+        </div>
+        {filteredEvents.map((event, index) => (
+          <Accordion key={index} expanded={expanded === index} onChange={handleChange(index)}>
+          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header" expandIcon={<ExpandMoreIcon />}>
+            <ListItem alignItems="flex-start"
+            secondaryAction={
+              <>
+                {/* <Button
                   variant="contained"
                   size="small"
                   onClick={() => handleRateEvent(event)}
                 >
                   Detail
-                </Button>
+                </Button> */}
                 <Button
                   variant="outlined"
                   size="small"
                   sx={{ mx: 1 }}
-                  onClick={() => handleEditClick(event)}
+                  onClick={(e) => handleEditClick(e, event)}
                 >
                   Edit
                 </Button>
@@ -344,17 +412,99 @@ const EventList = () => {
                   variant="contained"
                   color="error"
                   size="small"
-                  onClick={() => handleDeleteEvent(event._id)}
+                  onClick={(e) => handleDeleteEvent(e, event._id)}
                 >
                   Delete
                 </Button>
-              </ListItem>
-              <Divider />
-            </React.Fragment>
-          ))}
-        </List>
-      )}
-
+                </>
+            }>
+              <ListItemAvatar>
+                <DateAvatar date={event.date} />
+              </ListItemAvatar>
+                <ListItemText
+                  primary={
+                  <Grid container>
+                    <Grid>
+                      <Typography variant="h5">
+                        {event.title}
+                      </Typography>
+                    </Grid>
+                    <Grid>
+                      <Rating value={getAverage(event.rating) ?? 0} precision={0.1} readOnly size="small" sx={{mt: 1, ml: 1}}/>
+                    </Grid>
+                    <Grid>
+                      <Typography variant="h6" sx={{fontSize: '0.75rem', pt:1}}>
+                      {`(${event.rating.length})`}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  }
+                  secondary={
+                      <Typography
+                          component="span"
+                          variant="body2"
+                          color="textSecondary"
+                        >
+                          By: {event.club?.name} / Location: {event.location}
+                          <br />
+                      </Typography>
+                  }
+                />
+            </ListItem>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography variant="h6">
+            {event.description}
+            </Typography>
+            <Typography variant="caption">
+              Organizer: {event.organizer}
+            </Typography>
+            {auth.isAuthenticated() && <Paper sx={{m: 1, p:1}}>
+                <ListItem
+                  disableGutters
+                  secondaryAction={
+                    <IconButton aria-label="send" color="primary" onClick={()=>{handleRating(event)}}>
+                      <SendIcon  />
+                    </IconButton>
+                  }
+                >
+                  <Rating 
+                    name="controlled-rating" 
+                    value={rating} 
+                    precision={1} 
+                    onChange={(event, newValue) => {
+                      setRating(newValue);
+                    }}/>
+                </ListItem>
+                <TextField
+                  id="outlined-multiline-flexible"
+                  label="Comment"
+                  multiline
+                  maxRows={4}
+                  fullWidth
+                  placeholder="Leave your comment here"
+                  value={comment}
+                  onChange={(event) => {setComment(event.target.value)}}
+                />
+            </Paper>}
+            {event.rating && event.rating.length>0 && event.rating.map((item, i) => {
+              return (
+                  <div key={i}>
+                    <Divider sx={{mt:1, mb:1}}/>
+                    <Typography variant="subtitle1">
+                      {item.userName}
+                    </Typography>
+                    <Rating name="half-rating-read" value={item.stars} precision={1} readOnly size="small" sx={{mt: 1}}/>
+                    {item.comment && <Typography variant="body2">
+                      {item.comment}
+                    </Typography>}
+                  </div>
+              )})}
+          </AccordionDetails>
+        </Accordion>
+        ))}
+     
+    <Box sx={{ maxWidth: 600, margin: "auto", p: 2 }}>
       <Dialog open={Boolean(editingEvent)} onClose={handleCloseDialog}>
         <DialogTitle>Edit Event</DialogTitle>
         <DialogContent>
@@ -406,7 +556,7 @@ const EventList = () => {
             label="Club"
             name="club"
             fullWidth
-            value={updatedEvent.club || ""}
+            value={updatedEvent.club?._id}
             onChange={handleClubChange}
           >
             {clubs.map((club) => (
@@ -426,6 +576,8 @@ const EventList = () => {
         </DialogActions>
       </Dialog>
     </Box>
+    
+    </Container>
   );
 };
 
--- server/models/event.model.js
+++ server/models/event.model.js
@@ -1,96 +1,97 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const eventSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: [true, 'Event title is required.'],
-            trim: true, // Removes leading and trailing spaces
-            maxlength: [100, 'Title cannot exceed 100 characters.']
-        },
-        date: {
-            type: Date,
-            required: [true, 'Event date is required.'],
-            // validate: {
-            //     validator: (value) => value >= new Date(),
-            //     message: 'Event date cannot be in the past.'
-            // }
-        },
-        location: {
-            type: String,
-            required: [true, 'Event location is required.'],
-            trim: true
-        },
-        description: {
-            type: String,
-            required: false,
-            maxlength: [500, 'Description cannot exceed 500 characters.'],
-            trim: true
-        },
-        organizer: {
-            type: String,
-            required: [true, 'Organizer name is required.'],
-            trim: true
-        },
-        club: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Club', // Reference to the Club model
-            required: [true, 'Club is required.']
-        },
-        actions: {
-            type: String,
-            required: false, // Optional field
-            trim: true
-        },
-        rating: [{
-            user: {
-                type: String,
-                // required:true
-            },
-            stars: {
-                type: Number,
-                required: true, min: 1,
-                max: 5
-            },
-            review: {
-                type: String,
-                // required:true
-            }
-
-        }],
-        created: {
-
-            type: Date,
-            default: Date.now
-        },
-        updated: {
-            type: Date
-        }
-
+  {
+    title: {
+      type: String,
+      required: [true, "Event title is required."],
+      trim: true, // Removes leading and trailing spaces
+      maxlength: [100, "Title cannot exceed 100 characters."],
     },
-    {
-        timestamps: true, // Automatically add `createdAt` and `updatedAt` fields
-    }
+    date: {
+      type: Date,
+      required: [true, "Event date is required."],
+      // validate: {
+      //     validator: (value) => value >= new Date(),
+      //     message: 'Event date cannot be in the past.'
+      // }
+    },
+    location: {
+      type: String,
+      required: [true, "Event location is required."],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: false,
+      maxlength: [500, "Description cannot exceed 500 characters."],
+      trim: true,
+    },
+    organizer: {
+      type: String,
+      required: [true, "Organizer name is required."],
+      trim: true,
+    },
+    club: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Club", // Reference to the Club model
+      required: [true, "Club is required."],
+    },
+    actions: {
+      type: String,
+      required: false, // Optional field
+      trim: true,
+    },
+    rating: [
+      {
+        userId: {
+          type: String,
+        },
+        userName: {
+          type: String,
+        },
+        stars: {
+          type: Number,
+          required: true,
+          min: 1,
+          max: 5,
+        },
+        comment: {
+          type: String,
+        },
+      },
+    ],
+    created: {
+      type: Date,
+      default: Date.now,
+    },
+    updated: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true, // Automatically add `createdAt` and `updatedAt` fields
+  }
 );
 
 // Add custom error messages and constraints
-eventSchema.post('save', function (error, doc, next) {
-    if (error.name === 'ValidationError') {
-        const errors = Object.values(error.errors).map(err => err.message);
-        next(new Error(errors.join(', ')));
-    } else {
-        next(error);
-    }
+eventSchema.post("save", function (error, doc, next) {
+  if (error.name === "ValidationError") {
+    const errors = Object.values(error.errors).map((err) => err.message);
+    next(new Error(errors.join(", ")));
+  } else {
+    next(error);
+  }
 });
 
 // Pre-save middleware to update the `updated` field
-eventSchema.pre('save', function (next) {
-    if (!this.isNew) {
-        this.updated = Date.now();
-    }
-    next();
+eventSchema.pre("save", function (next) {
+  if (!this.isNew) {
+    this.updated = Date.now();
+  }
+  next();
 });
 
-const Event = mongoose.model('Event', eventSchema);
+const Event = mongoose.model("Event", eventSchema);
 
 export default Event;
