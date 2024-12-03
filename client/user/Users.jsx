38602668e63d275f8dddb7ff8ed322b3509451b8
Chiayi <angel05446a@gmail.com>
2024-12-03 18:43:43 -0500

    User Filter

--- client/user/Users.jsx
+++ client/user/Users.jsx
@@ -1,93 +1,109 @@
 import React from 'react'
-import {useState} from 'react'
-import {useEffect} from 'react'
-import { makeStyles } from '@mui/styles'
-import Paper from '@mui/material/Paper'
-import List from '@mui/material/List'
-import {list} from './api-user.js'
-import { Link as RouterLink } from 'react-router-dom';
-import Link from '@mui/material/Link'
+import {useState, useEffect} from 'react'
+import ArrowForward from '@mui/icons-material/ArrowForward';
+import Avatar from '@mui/material/Avatar'
+import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link'
+import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import ListItemText from '@mui/material/ListItemText'
-import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction'
-import IconButton from '@mui/material/IconButton'
-import Avatar from '@mui/material/Avatar'
+import Paper from '@mui/material/Paper'
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
 import Typography from '@mui/material/Typography'
-import ArrowForward from '@mui/icons-material/ArrowForward';
+import {list} from './api-user.js'
+import { Link as RouterLink } from 'react-router-dom';
 
-const useStyles = makeStyles(theme => ({
-    card: {
-      // Define your card styles here
-    },
-    textField: {
-      // Define your text field styles here
-    },
-    error: {
-      // Define your error icon styles here
-    },
-    submit: {
-      // Define your submit button styles here
-    },
-    title: {
-      // Define your title styles here
-    },
-    root: {
-        // Define your root styles here
-      },
-  }));
 
 export default function Users() {
 
+    const [name, setName] = useState("");
     const [users, setUsers] = useState([])
-useEffect(() => {
-    const abortController = new AbortController()
-    const signal = abortController.signal
-    list(signal).then((data) => {
-      console.log(data)
-if (data && data.error) { 
-console.log(data.error)
-} else { 
-    console.log(data)
-setUsers(data)
-} 
-})
-return function cleanup(){ 
-abortController.abort()
-} 
-}, [])
+    const [filterUsers, setFilterUsers] = useState([])
+    useEffect(() => {
+        const abortController = new AbortController()
+        const signal = abortController.signal
+        list(signal).then((data) => {
+          if (data && data.error) { 
+              console.log(data.error)
+          } else { 
+              setUsers(data)
+              setFilterUsers(data);
+          } 
+        })
+      return function cleanup(){ 
+        abortController.abort()
+      } 
+    }, [])
 
+    useEffect(() => {
+      let list = users;
+      if (name !== "") {
+        list = list.filter((user) => {
+          return user.name.toLowerCase().includes(name.toLowerCase());
+        });
+      }
+      setFilterUsers(list);
+    }, [name]);
   
-    const classes = useStyles()
-return (
-  <Container maxWidth="lg">
-<Paper className={classes.root} elevation={4}>
-<Typography variant="h6" className={classes.title}> 
-All Users
-</Typography> 
-<List dense>
-{users.map((item, i) => { 
-  return  <Link component={RouterLink} to={"/user/" + item._id} key={i}>
-    
-<ListItem> 
-<ListItemAvatar>
-<Avatar src={item.pictureUri} /> 
-
-</ListItemAvatar>
-<ListItemText primary={item.name}/> 
-<ListItemSecondaryAction>
-<IconButton>
-<ArrowForward/> 
-</IconButton>
-</ListItemSecondaryAction> 
-</ListItem>
-</Link> 
-})} 
-</List>
-</Paper>
-</Container>
-)
+    return (
+      <Container maxWidth="lg">
+        <ListItem alignItems="flex-start">
+        <ListItemText>
+          <Typography variant="h5" sx={{ mb: 2 }}>
+            User Management
+          </Typography>
+        </ListItemText>
+      </ListItem>
+      {/* Filter Section */}
+      <Box sx={{ display: "flex", gap: 2, marginBottom: 2 }}>
+        <TextField
+          label="Search by Name"
+          name="title"
+            size="small"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          fullWidth
+        />
+      </Box>
+      <div>
+          <Stack direction="row" sx={{ mb: 1 }}>
+            {name && (
+              <Chip
+                size="small"
+                name="title"
+                label={name}
+                variant="outlined"
+                onClick={() => setName("")}
+                onDelete={() => setName("")}
+              />
+            )}
+          </Stack>
+        </div>
+        <Paper elevation={4}>
+          <List dense>
+          {filterUsers.map((item, i) => { 
+            return  <Link component={RouterLink} to={"/user/" + item._id} key={i}>
+            <ListItem secondaryAction={
+              <IconButton>
+                <ArrowForward/> 
+              </IconButton>
+            }> 
+              <ListItemAvatar>
+                <Avatar src={item.pictureUri} /> 
+              </ListItemAvatar>
+              <ListItemText primary={item.name}/>
+            </ListItem>
+            </Link> 
+          })} 
+          </List>
+        </Paper>
+      </Container>
+    )
 }
 
 
