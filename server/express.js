d09142b140d213d7f0da567470140415ae60b089
YuHsuanChen <ddy20824@gmail.com>
2024-12-06 02:58:35 -0500

    Add CORS config

--- server/express.js
+++ server/express.js
@@ -10,8 +10,26 @@ import clubRoutes from './routes/club.routes.js'
 import eventRoutes from './routes/event.routes.js'
 import path from 'path'
 
+const allowedOrigins = [
+    'http://localhost:4173',
+    'http://localhost:5173',
+    'https://test-firebase-project-5f453.web.app/',
+]
+
 const app = express()
 const CURRENT_WORKING_DIR = process.cwd()
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true) // Allow the request
+        } else {
+            callback(new Error('Not allowed by CORS'), false) // Block the request
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Specify the allowed headers
+}))
+
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +42,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
-app.use(cors())
+
 app.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
         res.status(401).json({ "error": err.name + ": " + err.message })
@@ -33,5 +51,6 @@ app.use((err, req, res, next) => {
         console.log(err)
     }
 })
+
 
 export default app
